1f94d0594db29a53bc0f01f08a32c3a449f8ffef
BM <bmiller.pct@gmail.com>
2025-01-09 15:53:55 -0500

    Separated core interfaces into separate files.

---
+++ ToolScraper.Core/IEndMill.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToolScraper.Core;
+using ToolScraper.Core.Types;
+
+namespace ToolScraper.Core;
+
+/// <summary>
+/// Represents properties about a specialized type of tool: EndMill. 
+/// </summary>
+public interface IEndMill: ITool
+{
+    int FluteCount { get; init; } 
+    decimal CuttingDiameter { get; init; }  
+    decimal ShankDiameter { get; init; }
+    decimal EffectiveLength { get; init; }
+    decimal OverallLength { get; init; }
+    decimal CornerRadius { get; init; }
+
+}
---
+++ ToolScraper.Core/ITool.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToolScraper.Core;
+using ToolScraper.Core.Types;
+
+namespace ToolScraper.Core;
+
+/// <summary>
+/// Represents a generic tool object, a specialized form of a product. 
+/// </summary>
+public interface ITool: IProduct
+{
+    string Iso { get; init; }   
+    ToolTypes ToolType { get; init; }
+    WorkCenterTypes WorkCenterType { get;init; }
+    MaterialTypes Material { get; init; }
+    string MaterialDescription { get; init; }
+    bool Indexable { get; init; }
+}
