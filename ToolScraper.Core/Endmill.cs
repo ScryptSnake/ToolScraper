507ace6257a04f0ad538353821112ecce28d859c
BM <bmiller.pct@gmail.com>
2025-01-09 16:08:07 -0500

    Created a record type DTO for endmills.

---
+++ ToolScraper.Core/Endmill.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToolScraper.Core;
+using ToolScraper.Core.Types;
+
+/// <summary>
+/// A DTO to store information about an End Mill item. 
+/// </summary>
+public record EndMill(
+    // Product
+    string Id,
+    string Description,
+    string Country,
+    decimal Weight,
+    Uri? Url,
+
+    // Tool
+    ToolTypes ToolType,
+    WorkCenterTypes WorkCenterType,
+    MaterialTypes Material,
+    string MaterialDescription,
+    bool Indexable,
+
+    // End Mill
+    int FluteCount,
+    decimal CuttingDiameter,
+    decimal ShankDiameter,
+    decimal EffectiveLength,
+    decimal OverallLength,
+    decimal CornerRadius,
+    string Iso
+) : IEndMill;
