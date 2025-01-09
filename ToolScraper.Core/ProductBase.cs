1340dbbc0f360bb1575be2ee62e6ecf6380aad88
BM <bmiller.pct@gmail.com>
2025-01-09 15:01:26 -0500

    Created a few items to start the project. A ProductBase base class. And a ToolTypes enum.

---
+++ ToolScraper.Core/ProductBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolScraper.Core;
+
+public abstract class ProductBase
+{
+    public required string Id { get; init; }
+    public required string Country { get; init; }
+    public decimal Weight { get; init; }
+    public required string Description { get; init; }
+    public Uri? Url { get; init; }
+}
---
+++ ToolScraper.Core/Types/ToolTypes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolScraper.Core.Types
+{
+    public enum ToolTypes
+    {
+        Milling, Turning, Abrasive, Special
+
+    }
+}
