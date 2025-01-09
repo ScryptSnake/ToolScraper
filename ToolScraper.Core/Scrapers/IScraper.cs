b81db3da6cd6194fff0051eb7e4c8b216fb51f2b
BM <bmiller.pct@gmail.com>
2025-01-09 17:41:26 -0500

    Created an IScraper generic interface. This contract will enforce the contract for a scraper of a specific type.
    Created a ScraperResult<T> object to store scrape results.

---
+++ ToolScraper.Core/Scrapers/IScraper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolScraper.Core.Scrapers
+{
+
+    /// <summary>
+    /// A generic interface to which a Scraper for a specific type must adhere. 
+    /// </summary>
+    /// <typeparam name="T"> The object to scrape. </typeparam>
+    public interface IScraper<T>
+    {
+        public Uri Url { get; init; }
+
+        public ScraperResult<T> Scrape();
+
+        public Task<ScraperResult<T>> ScrapeAsync();
+
+    }
+}
---
+++ ToolScraper.Core/Scrapers/ScraperResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolScraper.Core.Scrapers
+{
+    // Returned from an IScraper<T>'s Scrape method. Encapsulated the result and a success boolean. 
+    public record ScraperResult<T>(bool Success, T Result)
+    {
+
+    }
+}
